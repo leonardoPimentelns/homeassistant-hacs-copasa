dff7690aedf2ce4e935b28bf6e6a5aa629278611
leonardoPimentelns <leonardooliveirapimentel@hotmail.com>
2022-11-21 21:57:05 -0300

    qrcode

--- custom_components/copasa/sensor.py
+++ custom_components/copasa/sensor.py
@@ -146,7 +146,7 @@ def get_open_invoices(config):
     invoices = json.loads(resp.content)
     barcode = invoices['faturas'][0]['numeroCodigoBarras']
     numeroFatura = invoices['faturas'][0]['numeroFatura']
-    valorUltimaFatura = get_paid_invoices()
+    valorUltimaFatura = get_paid_invoices(config)
     valorUltimaFatura =valorUltimaFatura['contas'][0]['valorTotalFatura'].replace('.','').replace(',','.')
     valorFatura = invoices['faturas'][0]['valorFatura'].replace('.','').replace(',','.')
     diferenca = (float(valorFatura) - float(valorUltimaFatura)) /(float(valorUltimaFatura))*100 
